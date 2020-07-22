a7c4ad0664e59ad3deedcb7a30d02074aa92371c
soleilyasmina <mounlasolomon@gmail.com>
2020-07-22 12:21:51 -0400

    all nibbles, create nibble and create bite connected and working

--- controllers/nibbles.js
+++ controllers/nibbles.js
@@ -1,16 +1,18 @@
 const Nibble = require('../models/nibble');
+const User = require('../models/user');
 
 // GET /nibbles/users/:user_id
 const allNibbles = async (req, res) => {
   try {
-    const { id } = res.locals.user;
-
-    return res.status(200).json('Reached!');
+    const nibbles = await Nibble
+      .find({ user_id: req.params.user_id })
+      .populate('ancestors')
+      .populate('contentAncestors');
+    return res.status(200).json({ nibbles });
   } catch (e) {
-
+    return res.status(500).json({ error: e.message });
   }
 };
-
 
 // GET /nibbles
 const myNibbles = async (req, res) => {
@@ -18,43 +20,59 @@ const myNibbles = async (req, res) => {
     const { id } = res.locals.user;
     return res.status(200).json('Reached!');
   } catch (e) {
-
+    return res.status(500).json({ error: e.message });
   }
-}
+};
 
 // GET /nibbles/:nibble_id
 const oneNibble = async (req, res) => {
   try {
     return res.status(200).json('Reached!');
   } catch (e) {
-
+    return res.status(500).json({ error: e.message });
   }
 };
 
 // POST /nibbles
 const newNibble = async (req, res) => {
   try {
-    return res.status(200).json('Reached!');
+    const nibble = await Nibble.create({ ...req.body, user_id: res.locals.user.id });
+    const user = await User.findById(res.locals.user.id);
+    user.nibbles.push(nibble._id);
+    return res.status(200).json({ nibble });
   } catch (e) {
-
+    return res.status(422).json({ error: e.message });
   }
 };
 
 // POST /nibbles/:nibble_id
 const newBite = async (req, res) => {
   try {
-    return res.status(200).json('Reached!');
+    const nibble = await Nibble.findById(req.params.nibble_id);
+    const bite = {
+      content: req.body.content,
+      user_id: res.locals.user.id,
+      ancestors: [...nibble.ancestors, nibble.id],
+      contentAncestors: nibble.content
+        ? [...nibble.contentAncestors, nibble.id]
+        : nibble.contentAncestors,
+    };
+    const createdBite = await Nibble.create(bite);
+    const user = await User.findById(res.locals.user.id);
+    user.nibbles.push(createdBite._id);
+    await user.save();
+    return res.status(200).json(createdBite);
   } catch (e) {
-
+    return res.status(422).json({ error: e.message });
   }
 };
 
 // PUT /nibbles/:nibble_id
 const updateNibble = async (req, res) => {
-  try { 
+  try {
     return res.status(200).json('Reached!');
   } catch (e) {
-
+    return res.status(500).json({ error: e.message });
   }
 };
 
@@ -63,7 +81,7 @@ const deleteNibble = async (req, res) => {
   try {
     return res.status(200).json('Reached!');
   } catch (e) {
-
+    return res.status(500).json({ error: e.message });
   }
 };
 
