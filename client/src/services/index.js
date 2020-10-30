a51318d820038329e387edf13f86854013639d2b
soleilyasmina <mounlasolomon@gmail.com>
2020-10-30 15:12:55 -0400

    update back end url

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api' : process.env.BACKEND_URL;
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api' : process.env.REACT_APP_BACKEND_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
