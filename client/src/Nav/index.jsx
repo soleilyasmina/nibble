41f87e8d3c1aaadc0268a076dfff820b266c48b6
soleilyasmina <mounlasolomon@gmail.com>
2020-10-30 14:31:23 -0400

    improve nav bar

--- client/src/Nav/index.jsx
+++ client/src/Nav/index.jsx
@@ -6,10 +6,12 @@ import { search } from "../services/users";
 const SiteNav = (props) => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchOptions, setSearchOptions] = useState([]);
+  const [expanded, setExpanded] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
     setSearchQuery('');
+    setExpanded(false);
   }, [location]);
 
   useEffect(() => {
@@ -23,25 +25,28 @@ const SiteNav = (props) => {
   }, [searchQuery]);
 
   return (
-    <Navbar sticky="top" bg="light" expand="md">
+    <Navbar sticky="top" bg="light" expand="md" expanded={expanded} onToggle={(e) => setExpanded(e)}>
       <Navbar.Brand><Link className="text-dark" to="/dashboard">Nibble</Link></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Link className="text-secondary" to={`/users/${props.user && props.user.id}`}>My Plate</Link>
+          <Link className="text-secondary mb-2 mb-md-0" to={`/users/${props.user && props.user.id}`}>My Plate</Link>
         </Nav>
-        <Form inline>
-          <FormControl value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}type="text" placeholder="Search" className="mr-sm-2" />
-          <ListGroup className="position-absolute" style={{ top: '47px', width: '193px' }}>
-            {searchOptions.map((opt, i) => (
-              <ListGroupItem>
-                <Link className="font-weight-bold text-dark" to={`/users/${opt._id}`}>
-                  {opt.username}
-                </Link>
-              </ListGroupItem>
-            ))}
-          </ListGroup>
-        </Form>
+        {props.user && (
+          <Form inline>
+            <FormControl value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} type="text" placeholder="Search" className="mr-sm-2" />
+            <ListGroup className="position-absolute nav-options">
+              {searchOptions.map((opt, i) => (
+                <ListGroupItem>
+                  <Link className="font-weight-bold text-dark" to={`/users/${opt._id}`}>
+                    {opt.username}
+                  </Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Form>
+        )
+        }
       </Navbar.Collapse>
     </Navbar>
   )
--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api' : 'https://14ab94abd684.ngrok.io';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api' : process.env.BACKEND_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
