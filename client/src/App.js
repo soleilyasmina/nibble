b7462c10740da92e2a67a9f0d7644cbcd6ccf014
soleilyasmina <mounlasolomon@gmail.com>
2020-10-30 22:04:02 -0400

    additional bug fixing

--- client/src/App.js
+++ client/src/App.js
@@ -18,18 +18,23 @@ function App() {
 
   useEffect(() => {
     if (user) {
+      if (user.error) {
+        localStorage.removeItem('token');
+        history.push("/login");
+      } else {
       (async () => {
         setFollowing(await followingNibbles());
       })();
       if (!location.pathname.includes("/users/")) {
         history.push("/dashboard");
       }
+      }
     } else if (localStorage.getItem("token")) {
       (async () => {
         setUser(await verify());
       })();
     } else {
-      localStorage.clear();
+      localStorage.removeItem('token');
       history.push("/login");
     }
   }, [user, toggleFollowing, history]); // eslint-disable-line
